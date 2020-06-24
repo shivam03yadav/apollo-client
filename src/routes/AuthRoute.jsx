f7dc0fcd0e269a737167d9082114e5373ec4d914
shivam03yadav <shivam.yadav@successive.tech>
2020-06-24 19:20:23 +0530

    feat: add files

--- src/routes/AuthRoute.jsx
+++ src/routes/AuthRoute.jsx
@@ -1,10 +1,10 @@
+
 /* eslint-disable react/forbid-prop-types */
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { AuthLayout } from '../layouts';
-
+import { AuthLayout } from '../layouts/index';
 
 const AuthRoute = ({ component: Component, ...rest }) => (
   <Route
